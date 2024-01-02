a695a1d2e0c14be16690b9b627b310f363bcc1b9
LucianeC <costaluciane087@gmail.com>
2024-01-01 23:38:03 -0300

    commit 2

--- src/main/kotlin/fundamentos/classe.kt
+++ src/main/kotlin/fundamentos/classe.kt
@@ -1,4 +1,13 @@
 package fundamentos
 
-class classe {
+//construtor
+class Pessoa (var nome: String, var idade: Int ) {
+    override fun toString(): String {
+        return "Nome da classe: 'Pessoa' Nome: ${nome}, Idade: ${idade}"
+    }
+}
+
+fun main(){
+    var luciane = Pessoa("Luciane", 19)
+    println(luciane)
 }
